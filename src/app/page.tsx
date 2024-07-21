51b7e008947eaa77edee1672b3f88a82c63a70ac
Yash Garg <thisisyashgarg@gmail.com>
2024-07-21 21:49:37 +0530

    qol: added skeleton structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,117 @@
-import Image from "next/image";
-
-export default function Home() {
+const ReportPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <div className="space-y-6 border-2 w-2/3 border-red-500 m-10 p-6 pb-8">
+      <div className="flex justify-between">
+        <h1>LiveFlow</h1>
+        <p>(icon)</p>
+      </div>
+
+      <section className="space-y-4">
+        <p className="text-center w-full">
+          You have 1 day remaining in your trial period. Pick a plan here.
+          <span>(icon)</span>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+        <div className="flex border-b">
+          <button>Ujjwal Singh</button>
+          <button>(icon) Create report</button>
+          <button>(icon) Share report</button>
+          <button>(icon) Manage report</button>
+          <button>(icon) Drill down</button>
+          <button>(icon) Consolidation</button>
+        </div>
+
+        <div className="flex">
+          <button>Start from scratch</button>
+          <button>Start from a template</button>
+          <button>Import from spreadsheet</button>
+          <button>Create consolidated report</button>
+        </div>
+      </section>
+
+      <form className="flex flex-col gap-6">
+        <div className="flex flex-col">
+          <label>Select company</label>
+          <div className="flex justify-between w-full">
+            <select className="basis-4/5">
+              <option>Demo Company</option>
+            </select>
+            <button className="basis-1/5">Connect another company</button>
+          </div>
+
+          <p>
+            Heads up! You're about to add a report for Demo Company to a
+            spreadsheet already containing reports for 33
+          </p>
+        </div>
+
+        <div className="flex w-full">
+          <div className="flex flex-col basis-4/5">
+            <label>Choose a report</label>
+            <select>
+              <option>Profit & Loss</option>
+            </select>
+            <p>You can insert rows and columns directly inside your report</p>
+          </div>
+
+          <div className="flex flex-col basis-1/5">
+            <label>Accounting Method</label>
+            <div className="flex">
+              <div className="flex">
+                <input
+                  type="radio"
+                  id="account"
+                  name="options"
+                  value="account"
+                />
+                <label htmlFor="account">Account</label>
+              </div>
+              <div className="flex">
+                <input type="radio" id="cash" name="options" value="cash" />
+                <label htmlFor="cash">Cash</label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div className="flex flex-col basis-1/2">
+            <label>Choose a date range</label>
+            <select>
+              <option>This year</option>
+            </select>
+          </div>
+          <div className="flex flex-col basis-1/2">
+            <label>Display columns by</label>
+            <select>
+              <option>Month</option>
+            </select>
+          </div>
+        </div>
+
+        <label>
+          <input type="checkbox" />
+          <span>Add budget & variance columns</span>
+        </label>
+      </form>
+
+      <div className="flex justify-center">
+        <div className="flex">
+          <button>Advanced settings</button>
+          <button>Formatting</button>
+          <button>Filters (0/5)</button>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      <footer className="flex">
+        <button>(icon) Create Report</button>
+        <div>
+          <h2>Sheet name</h2>
+          <p>Profit and loss (icon)</p>
+        </div>
+      </footer>
+    </div>
+  )
 }
+
+export default ReportPage
